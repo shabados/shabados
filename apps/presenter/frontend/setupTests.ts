shabados/presenter

test: move isMac const mock to a dedicated consts mock file

apps/frontend/setupTests.ts
@@ -1,3 +1,5 @@
 import '@testing-library/jest-dom'
 
+jest.mock( './src/lib/consts' )
+
 export default {}

shabados/shabados

apps/presenter/frontend/setupTests.ts
@@ -1,3 +1,5 @@
 import '@testing-library/jest-dom'
 
+jest.mock( './src/helpers/consts' )
+
 export default {}