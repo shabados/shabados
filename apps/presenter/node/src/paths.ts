6f4e50b9a3f493ea3be1bcf5c65e1e6f5df868e9
Harjot Singh <harjot@harkul.com>
2024-11-10 20:07:57 +0000

    build: update root dev dependencies

--- apps/presenter/node/src/paths.ts
+++ apps/presenter/node/src/paths.ts
@@ -3,9 +3,10 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 import getAppDataPath from 'appdata-path'
+import { resolve } from 'import-meta-resolve'
 
 export const resolveModule = ( name: string ) => dirname( fileURLToPath(
-  import.meta.resolve( name )
+  resolve( name, import.meta.url )
 ) )
 
 export const DATA_FOLDER = join( getAppDataPath(), 'Shabad OS' )
